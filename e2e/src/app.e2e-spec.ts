8e3d1df640b7035539b76d607c55014f3617bd84
Jannes Samuel Magnusson <j.magnusson@campus.tu-berlin.de>
2020-03-22 18:14:36 +0100

    update to angular 9

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to papergames!');
+    expect(page.getTitleText()).toEqual('papergames app is running!');
   });
 
   afterEach(async () => {
